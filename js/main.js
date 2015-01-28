821c6642fc9a56dd6a7bbe12b20a9b379b54e256
Joomin Yeo <joominyeo@outlook.com>
2015-01-27 18:54:14 -0800

    popup working, text not showing
    switch code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 jQuery(document).ready(function($){
+
+	window.onload = function(){
+		$('.cd-popup').addClass('is-visible');
+	}
+
 	//open popup
 	$('.cd-popup-trigger').on('click', function(event){
 		event.preventDefault();
 		$('.cd-popup').addClass('is-visible');
 	});
-	
+
 	//close popup
 	$('.cd-popup').on('click', function(event){
 		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
